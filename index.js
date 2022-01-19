b9d2ff2347bddd82238b65b1d3f20f0dcd02050e
Cannen Hanzik <cannenhanzik2@gmail.com>
2022-01-19 12:48:20 -0600

    initial commit. Did task 1 and started task2

--- index.js
+++ index.js
@@ -39,15 +39,47 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
 }
 
 
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
 
+const sallie = new Person('Sallie', 26);
+const mark = new Person('Mark', 23);
+const cori = new Person('Cori', 25);
 
+console.log(sallie.toString());
+console.log(mark.toString());
+console.log(cori.toString());
 
+mark.eat('Pizza');
+mark.eat('Pasta');
+mark.eat('Tacos');
+mark.eat('Sushi');
+mark.eat('Sammich');
+mark.eat('cake');
+mark.eat('ramen');
+
+console.log(mark.stomach);
+mark.poop();
+console.log(mark.stomach);
 
 /*
   TASK 2
